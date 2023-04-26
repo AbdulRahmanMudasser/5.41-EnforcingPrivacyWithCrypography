d8eed6da99bdd59991e3e79c7c359aa4dfb62d1d
Abdul Rahman <abdulrahmanmuddasser@gmail.com>
2023-04-26 21:48:42 +0500

    swapping in encryption

--- Enforcing Privacy With Crypography/Program.cs
+++ Enforcing Privacy With Crypography/Program.cs
@@ -1,7 +1,11 @@
-﻿class Cryptography
+﻿using System.Runtime.ExceptionServices;
+
+class Cryptography
 {
     static void Main(string[] args)
     {
+        int temp;   // varibale for swapping
+        
         Console.Write("Enter Four Digit Number:  ");
         int num = Convert.ToInt32(Console.ReadLine());
 
@@ -10,6 +14,16 @@
         int digit2 = ((num % 1000) / 100 + 7) % 10;
         int digit3 = ((num % 100) / 10 + 7) % 10;
         int digit4 = ((num % 10) + 7) % 10;
+
+        // swapping digit1 with digit3
+        temp = digit1;
+        digit1 = digit3;
+        digit3 = temp;
+
+        // swapping digit2 with digit4
+        temp = digit2;
+        digit2 = digit4;
+        digit4 = temp;
 
         int encrypted = (digit1 * 1000) + (digit2 * 100) + (digit3 * 10) + digit4;
 
